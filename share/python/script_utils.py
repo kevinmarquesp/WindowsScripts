ddfa0e72dca7f41fe7cfacd43076f7c1bd2cb92d
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-08 11:06:17 -0300

    :recycle: refactoring (share.py): removed the script utils class and put all function on the root of the file

--- share/python/script_utils.py
+++ share/python/script_utils.py
@@ -1,107 +1,169 @@
-from typing import Any
-import curses
+from typing import Any, Literal, Optional
+import curses, os
 
 
-class ScriptUtils:
-    @staticmethod
-    def display_options_menu(title: str, options: dict[Any, str], default_option: int = 0,
-                             up_keys: list[int] = [curses.KEY_UP, ord("k")],
-                             down_keys: list[int] = [curses.KEY_DOWN, ord("j")],
-                             select_keys: list[int] = [curses.KEY_ENTER, 10, 13, ord("o")]) -> Any:
+def display_options_menu(title: str, options: dict[Any, str], default_option: int = 0,
+                            up_keys: list[int] = [curses.KEY_UP, ord("k")],
+                            down_keys: list[int] = [curses.KEY_DOWN, ord("j")],
+                            select_keys: list[int] = [curses.KEY_ENTER, 10, 13, ord("o")]) -> Any:
+    r"""
+    This function displays an interactive options menu in the terminal. The menu is navigable using specified keys
+    for moving the selection up and down, and for selecting an option.
+
+    :param title:
+        The title displayed at the top of the options menu.
+    :param options:
+        A dictionary where each key-value pair represents an option. The keys are option identifiers and the
+        values are the prompts displayed for each option.
+    :param default_option:
+        The index of the option that is selected by default when the menu is displayed. If not provided, the first
+        option is selected by default.
+    :param up_keys:
+        A list of key codes that, when pressed, move the selection up in the menu. By default, these are the up
+        arrow key and the 'k' key.
+    :param down_keys:
+        A list of key codes that, when pressed, move the selection down in the menu. By default, these are the
+        down arrow key and the 'j' key.
+    :param select_keys:
+        A list of key codes that, when pressed, select the currently highlighted option. By default, these are the
+        enter key and the 'o' key.
+
+    :return:
+        A tuple containing three elements: the index of the selected option, the key of the selected option, and
+        the value of the selected option. This allows the calling code to know which option was selected by the
+        user.
+    """
+
+    TITLE_PADDING_START: int = 1
+    PROMPT_PADDING_START: int = 2
+
+    option_keys: list[Any] = list(options.keys())
+    option_values: list[str] = list(options.values())
+
+    #variables related to formating the style of the menu, size of each option, paddings, etc.
+
+    max_prompt_length: int = max(list(map(len, option_values)))
+    title_lines: list[str] = title.split("\n")
+    title_padding_top: int = len(title_lines) - 1
+    menu_list_padding_top: int = title_padding_top + len(title_lines) + 1
+
+    #variables that the wrapper function will use to know witch option is selected
+
+    selected_index: int = default_option
+    selected_key: Any = option_keys[selected_index]
+    selected_value: str = option_values[selected_index]
+
+    user_input: int = 0
+
+    def __display_options_menu_wrapper(stdscr: "curses._CursesWindow"):
         r"""
-        This function displays an interactive options menu in the terminal. The menu is navigable using specified keys
-        for moving the selection up and down, and for selecting an option.
+        This function displays an options menu in a curses window. It allows the user to navigate through the
+        options using the up and down keys.
 
-        :param title:
-            The title displayed at the top of the options menu.
-        :param options:
-            A dictionary where each key-value pair represents an option. The keys are option identifiers and the
-            values are the prompts displayed for each option.
-        :param default_option:
-            The index of the option that is selected by default when the menu is displayed. If not provided, the first
-            option is selected by default.
-        :param up_keys:
-            A list of key codes that, when pressed, move the selection up in the menu. By default, these are the up
-            arrow key and the 'k' key.
-        :param down_keys:
-            A list of key codes that, when pressed, move the selection down in the menu. By default, these are the
-            down arrow key and the 'j' key.
-        :param select_keys:
-            A list of key codes that, when pressed, select the currently highlighted option. By default, these are the
-            enter key and the 'o' key.
-
-        :return:
-            A tuple containing three elements: the index of the selected option, the key of the selected option, and
-            the value of the selected option. This allows the calling code to know which option was selected by the
-            user.
+        :param stdscr:
+            The curses window in which the options menu will be displayed.
+        :type stdscr:
+            curses._CursesWindow
         """
 
-        TITLE_PADDING_START: int = 1
-        PROMPT_PADDING_START: int = 2
+        nonlocal title_lines, option_keys, option_values, selected_key, selected_index, selected_value, user_input
 
-        option_keys: list[Any] = list(options.keys())
-        option_values: list[str] = list(options.values())
+        stdscr.refresh()
 
-        #variables related to formating the style of the menu, size of each option, paddings, etc.
+        while user_input not in select_keys:  #only stops the event listener when the enter key is pressed
+            stdscr.clear()
 
-        max_prompt_length: int = max(list(map(len, option_values)))
-        title_lines: list[str] = title.split("\n")
-        title_padding_top: int = len(title_lines) - 1
-        menu_list_padding_top: int = title_padding_top + len(title_lines) + 1
+            for key, title_line in enumerate(title_lines):  #logic to write the title lines
+                stdscr.addstr(title_padding_top + key, TITLE_PADDING_START + PROMPT_PADDING_START, title_line)
 
-        #variables that the wrapper function will use to know witch option is selected
+            for key, option_value in enumerate(option_values):  #logic to write the menu options formated in a beaty way
+                option_length: int = len(option_value)
+                option_length_difference: int = max_prompt_length - option_length
+                option_prompt: str = f"  {option_value.strip()}  " + " " * option_length_difference
 
-        selected_index: int = default_option
-        selected_key: Any = option_keys[selected_index]
-        selected_value: str = option_values[selected_index]
+                stdscr.addstr(key + menu_list_padding_top, PROMPT_PADDING_START, option_prompt,
+                                curses.A_REVERSE if key == selected_index else 0)
 
-        user_input: int = 0
+                if key == len(option_values) - 1:
+                    stdscr.addstr(key + menu_list_padding_top + 2, 0, "")
 
-        def __display_options_menu_wrapper(stdscr: "curses._CursesWindow"):
-            r"""
-            This function displays an options menu in a curses window. It allows the user to navigate through the
-            options using the up and down keys.
-
-            :param stdscr:
-                The curses window in which the options menu will be displayed.
-            :type stdscr:
-                curses._CursesWindow
-            """
-
-            nonlocal title_lines, option_keys, option_values, selected_key, selected_index, selected_value, user_input
+            #user event listeners section
 
             stdscr.refresh()
+            user_input = stdscr.getch()
 
-            while user_input not in select_keys:  #only stops the event listener when the enter key is pressed
-                stdscr.clear()
+            if user_input in up_keys:
+                selected_index = (selected_index - 1) % len(option_keys)
+            elif user_input in down_keys:
+                selected_index = (selected_index + 1) % len(option_keys)
 
-                for key, title_line in enumerate(title_lines):  #logic to write the title lines
-                    stdscr.addstr(title_padding_top + key, TITLE_PADDING_START + PROMPT_PADDING_START, title_line)
+            selected_key = option_keys[selected_index]
+            selected_value = option_values[selected_index]
 
-                for key, option_value in enumerate(option_values):  #logic to write the menu options formated in a beaty way
-                    option_length: int = len(option_value)
-                    option_length_difference: int = max_prompt_length - option_length
-                    option_prompt: str = f"  {option_value.strip()}  " + " " * option_length_difference
+    curses.wrapper(__display_options_menu_wrapper)
+    return (selected_index, selected_key, selected_value)
 
-                    stdscr.addstr(key + menu_list_padding_top, PROMPT_PADDING_START, option_prompt,
-                                  curses.A_REVERSE if key == selected_index else 0)
 
-                    if key == len(option_values) - 1:
-                        stdscr.addstr(key + menu_list_padding_top + 2, 0, "")
+def cprint(message: str) -> None:
+    r"""
+    Prints a colored message to the console based on color tags within the message.
 
-                #user event listeners section
+    :param message: The message to print with color tags.
+    """
 
-                stdscr.refresh()
-                user_input = stdscr.getch()
+    clr_tags: list[tuple[str, str]] = [
+        ("[B]", "\033[;;30m"), ("[^B]", "\033[;;40m"),
+        ("[r]", "\033[;;31m"), ("[^r]", "\033[;;41m"),
+        ("[g]", "\033[;;32m"), ("[^g]", "\033[;;42m"),
+        ("[y]", "\033[;;33m"), ("[^y]", "\033[;;43m"),
+        ("[b]", "\033[;;34m"), ("[^b]", "\033[;;44m"),
+        ("[m]", "\033[;;35m"), ("[^m]", "\033[;;45m"),
+        ("[c]", "\033[;;36m"), ("[^c]", "\033[;;46m"),
+        ("[w]", "\033[;;37m"), ("[^w]", "\033[;;47m"),
+        ("[/]", "\033[m"),
+    ]
 
-                if user_input in up_keys:
-                    selected_index = (selected_index - 1) % len(option_keys)
-                elif user_input in down_keys:
-                    selected_index = (selected_index + 1) % len(option_keys)
+    for clr_tag, clr_code in clr_tags:
+        message = message.replace(clr_tag, clr_code)
 
-                selected_key = option_keys[selected_index]
-                selected_value = option_values[selected_index]
+    print(message)
 
-        curses.wrapper(__display_options_menu_wrapper)
-        return (selected_index, selected_key, selected_value)
-#end: ScriptUtils
+
+logger_msg_type = Literal["warning", "info", "todo", "error", "fail", "good", "pass"]
+logger_pad_type = Literal["top", "bottom", "both"]
+
+def logger(message: str, ptype: logger_msg_type = "info", padding: Optional[logger_pad_type] = None) -> None:
+    r"""
+    Logs a message with a specified type and optional padding.
+
+    :param message:
+        The message to log.
+    :param ptype:
+        The type of the log message. Defaults to "info".
+    :param padding:
+        The type of padding to apply. Can be "top", "bottom", or "both". Defaults to None.
+    """
+
+    prefix: dict[logger_msg_type, str] = {
+        "warning": "\033[43m WARN \033[;;33m",
+        "info":    "\033[46m INFO \033[;;36m",
+        "todo":    "\033[44m TODO \033[;;34m",
+        "error":   "\033[41m ERRO \033[;;31m",
+        "fail":    "\033[41m FAIL \033[;;31m",
+        "good":    "\033[42m GOOD \033[;;32m",
+        "pass":    "\033[42m PASS \033[;;32m"
+    }
+
+    padding_top: str = "\n" if padding in ["top", "both"] else ""
+    padding_bottom: str = "\n" if padding in ["bottom", "both"] else ""
+
+    print(f"{padding_top}{prefix[ptype]} {message}\033[m{padding_bottom}")
+
+
+def clear() -> None:
+    r"""
+    Clears the stdout screen.
+    """
+
+    os.system("cls" if os.name == "nt" else "clear")
+
